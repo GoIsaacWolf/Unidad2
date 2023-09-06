f915d4775e14b9828963c511695fcc9ccd058416
GoIsaacWolf <143622297+GoIsaacWolf@users.noreply.github.com>
2023-09-06 08:42:34 -0600

    Add files via upload

---
+++ Lista Tareas.java
@@ -0,0 +1,22 @@
+
+package javaapplication8;
+
+public class NewClass {
+    public static void main(String[] args) {
+    String tarea ="laver trastes";
+    String tarea2="Laver Ropa";
+    String tarea3="Barrer";
+    String tarea4="Trapear";
+    String tarea5="Estudiar Programacion";
+    
+    
+  
+    
+    System.out.println(" mi tarea  es :" + tarea);
+    System.out.println("mi tarea es :" + tarea2);
+        System.out.println("mi tarea es :"+ tarea3);
+        System.out.println("mi tarea es :"+ tarea4 );
+        System.out.println("mi tarea es :" + tarea5);
+    }
+   
+}
