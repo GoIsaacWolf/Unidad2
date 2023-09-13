61997b9badf7cec7aa748a22446211cec4530946
GoIsaacWolf <143622297+GoIsaacWolf@users.noreply.github.com>
2023-09-13 08:43:41 -0600

    Add files via upload

---
+++ Areadeltriangulo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.areadeltriangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Areadeltriangulo {
+
+    public static void main(String[] args) {
+        //numeros enteros
+        //numeros decimales
+        int base =5;
+        int altura = 10;
+        int area = (base*altura)/2;
+        
+        
+        System.out.println("El area del triangulo es :" + area);
+    
+   }
+}
